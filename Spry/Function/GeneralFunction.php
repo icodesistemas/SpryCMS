229083b0d9657c9ac1ce8e1711ad21d39fda4a54
iCode <icode.sistemas@gmail.com>
2016-04-19 20:23:06 -0430

    funcion para convertir segundos a hora minutos y segudos

--- Spry/Function/GeneralFunction.php
+++ Spry/Function/GeneralFunction.php
@@ -12,7 +12,6 @@
 			$horas = floor($tiempo_en_segundos / 3600);
 			$minutos = floor(($tiempo_en_segundos - ($horas * 3600)) / 60);
 			$segundos = $tiempo_en_segundos - ($horas * 3600) - ($minutos * 60);
-
 			return $horas . ':' . $minutos . ":" . $segundos;
 		}
 		public  function getCrearUrl($str){
